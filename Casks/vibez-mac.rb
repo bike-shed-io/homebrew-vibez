6fdac40c50c1f654967c883d56841659cbfbb895
Patrick Oberherr <patrick.oberherr@gmail.com>
2026-04-21 22:45:33 +0200

    feat: add Homebrew cask for vibez mac app

---
+++ Casks/vibez-mac.rb
@@ -0,0 +1,17 @@
+cask "vibez-mac" do
+  version "0.1.0"
+  sha256 "3f5a648927f8151e0f798cc4cc81440e484f4f891ba6a6b0806042e109ff4c28"
+
+  url "https://github.com/bike-shed-io/vibez-mac/releases/download/v#{version}/Vibez-macos-arm64.zip"
+  name "Vibez"
+  desc "Native menu bar app for Vibez radio"
+  homepage "https://github.com/bike-shed-io/vibez-mac"
+
+  app "Vibez.app"
+
+  zap trash: [
+    "~/Library/Preferences/io.bike-shed.vibez.mac.plist",
+    "~/Library/HTTPStorages/io.bike-shed.vibez.mac",
+    "~/Library/WebKit/io.bike-shed.vibez.mac"
+  ]
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# vibez-mac
+
+Homebrew tap and release repository for the native Vibez macOS app.
+
+## Install
+
+```sh
+brew tap bike-shed-io/vibez-mac
+brew install --cask vibez-mac
+```
+
+## Upgrade
+
+```sh
+brew update
+brew upgrade --cask vibez-mac
+```
